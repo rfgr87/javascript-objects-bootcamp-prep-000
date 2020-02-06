f1412f16efba0367bf07c9464e73da5b45ae9713
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-06 00:33:10 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artist_name, song){
 }
 
 function removeFromPlaylist(playlist, artist_name){
-  delete playlist.artist_name;
+  delete playlist[artist_name];
   return playlist
 }
